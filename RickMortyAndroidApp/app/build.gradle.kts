4ee07459561d268dc48ccf8250e12741be36fee4
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2021-12-13 21:25:07 -0300

    multi-module #COMMENT * Settings to multi-module build setup

--- RickMortyAndroidApp/app/build.gradle.kts
+++ RickMortyAndroidApp/app/build.gradle.kts
@@ -1,56 +1,7 @@
-import org.jetbrains.kotlin.cli.jvm.main
-
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-kapt")
-}
-
 android {
-    compileSdk = AppConfig.compileSdk
-
-    defaultConfig {
-        applicationId = AppConfig.applicationId
-        minSdk = AppConfig.minSdk
-        targetSdk = AppConfig.targetSdk
-        versionCode = AppConfig.versionCode
-        versionName = AppConfig.versionName
-
-        testInstrumentationRunner = AppConfig.testInstrumentationRunner
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-
     buildFeatures {
         dataBinding = true
         viewBinding = true
-    }
-
-    // TODO: aplicar em subprojects???
-    sourceSets.getByName("main") {
-        java.srcDir("src/main/kotlin")
-        java.srcDirs("src/main/kotlin")
-    }
-    sourceSets.getByName("test") {
-        java.srcDir("src/test/kotlin")
-        java.srcDir("src/test/kotlin")
     }
 }
 
--- RickMortyAndroidApp/network/build.gradle.kts
+++ RickMortyAndroidApp/network/build.gradle.kts
@@ -1,33 +1,6 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    id("kotlinx-serialization")
-}
-
 android {
-    compileSdk = AppConfig.compileSdk
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
     defaultConfig {
         buildConfigField("String", "BASE_URL", "\"https://rickandmortyapi.com/api/\"")
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-
-    sourceSets.getByName("main") {
-        java.srcDir("src/main/java")
-        java.srcDirs("src/main/kotlin")
-    }
-    sourceSets.getByName("test") {
-        java.srcDir("src/test/java")
-        java.srcDir("src/test/kotlin")
     }
 }
 
