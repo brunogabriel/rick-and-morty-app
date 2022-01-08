a0f90adecc6dfda700bcb8a6f5cbf0d032b70a5e
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2022-01-08 20:28:09 -0300

    episodes #COMMENT * Fixing loading_view.xml

--- RickMortyAndroidApp/style-guide/src/main/kotlin/io/github/brunogabriel/rickmorty/styleguide/components/LoadingView.kt
+++ RickMortyAndroidApp/style-guide/src/main/kotlin/io/github/brunogabriel/rickmorty/styleguide/components/LoadingView.kt
@@ -4,12 +4,13 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import io.github.brunogabriel.rickmorty.styleguide.databinding.LoadingViewBinding
 
 class LoadingView @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null
-) : LinearLayout(context, attributeSet) {
+) : RelativeLayout(context, attributeSet) {
 
     private val binding: LoadingViewBinding =
         LoadingViewBinding.inflate(LayoutInflater.from(context), this, true)
