4ba149c8894817253a027f78af3bc5e584411178
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2022-01-08 20:40:41 -0300

    episodes #COMMENT * Fixing TryAgainView.kt

--- RickMortyAndroidApp/style-guide/src/main/kotlin/io/github/brunogabriel/rickmorty/styleguide/components/TryAgainView.kt
+++ RickMortyAndroidApp/style-guide/src/main/kotlin/io/github/brunogabriel/rickmorty/styleguide/components/TryAgainView.kt
@@ -3,13 +3,13 @@ package io.github.brunogabriel.rickmorty.styleguide.components
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import io.github.brunogabriel.rickmorty.styleguide.databinding.TryAgainViewBinding
 
 class TryAgainView @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null
-) : LinearLayout(context, attributeSet) {
+) : RelativeLayout(context, attributeSet) {
 
     private val binding: TryAgainViewBinding =
         TryAgainViewBinding.inflate(LayoutInflater.from(context), this, true)
